c7560e546b8aeba6570274f3e800bb3f574d14dc
YoussefKadaouiAbbassi <Youssefabbassi4@gmail.com>
2024-05-25 18:26:48 +0200

    fix (front): remove unused components

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,12 +1,5 @@
 import MainHeader from '@/components/headers/mainHeader';
 
 export default async function Index() {
-  return (
-    <div className="flex-1 w-full flex flex-col gap-20 items-center">
-      <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16" />
-      <div className="animate-in flex-1 flex flex-col gap-20 opacity-0 max-w-4xl px-3">
-        <MainHeader />
-      </div>
-    </div>
-  );
+  return <MainHeader />;
 }
